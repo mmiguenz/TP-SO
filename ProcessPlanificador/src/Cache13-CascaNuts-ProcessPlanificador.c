0e329c74c399511b73d894bb71b3ed16bd2e29a1
SebaAgosta <seba.agosta@gmail.com>
2015-08-31 11:49:15 -0300

    Creo el proyecto base Planificador para empezar a probar y codear cosas

---
+++ ProcessPlanificador/src/Cache13-CascaNuts-ProcessPlanificador.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Cache13-CascaNuts-ProcessPlanificador.c
+ Author      : GrupoCascaNuts
+ Version     :
+ Copyright   : Ante Cualquier copia se le formateara la pc ;)
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hola Vieja.. este es uno de los procesos bases!!"); /* prints !!!Hola Vieja!! */
+	return EXIT_SUCCESS;
+}
