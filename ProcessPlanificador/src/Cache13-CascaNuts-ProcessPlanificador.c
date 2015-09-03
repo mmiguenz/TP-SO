37fb920cbd2dc862584bb03fb75e16faa6bea318
SebaAgosta <seba.agosta@gmail.com>
2015-09-03 16:13:04 -0300

    Creo el primer log

--- ProcessPlanificador/src/Cache13-CascaNuts-ProcessPlanificador.c
+++ ProcessPlanificador/src/Cache13-CascaNuts-ProcessPlanificador.c
@@ -10,8 +10,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <string.h>
+#include <commons/log.h>
+
+/** Puerto  */
+#define PORT       7000
+
+
+/** Longitud del buffer  */
+#define BUFFERSIZE 512
 
 int main(void) {
-	puts("!!!Hola Vieja.. este es uno de los procesos bases!!"); /* prints !!!Hola Vieja!! */
+	t_log* Planificador;
+	Planificador = log_create("Nuevo", "Panificador", 1, LOG_LEVEL_INFO) ; //Creo el primer archivo de log del proceso planificador
+	log_trace(Planificador, "Primer Log");//Porque no hace nada?
+	puts("!!!Planificacion!!"); /* prints !!!Planificacion!! */
 	return EXIT_SUCCESS;
 }
