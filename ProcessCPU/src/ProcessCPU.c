665dc8fc327e2b58b87fd2be8cab4c7cb937aff8
Florencia Lopez <florenciamacarenalopez@hotmail.com.ar>
2015-09-10 20:31:56 -0300

    subo cambios en cpu

--- ProcessCPU/src/ProcessCPU.c
+++ ProcessCPU/src/ProcessCPU.c
@@ -31,72 +31,46 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
+#include <string.h>
 #include <commons/config.h>
 #include <commons/log.h>
 #include <commons/string.h>
 
-
-
-
-
-/** Longitud del buffer  */
-#define BUFFERSIZE 512
-
 int main(void) {
-//	t_log* Planificador;
-	//Planificador = log_create("Nuevo_Log", "Panificador", 1, LOG_LEVEL_INFO) ; //Creo el primer archivo de log del proceso planificador
-//	log_debug(Planificador, "Primer Log");//Porque no hace nada?
-//	log_impl_template(log_info, LOG_LEVEL_INFO);
-
-
-
-    struct sockaddr_in dire_memoria;
+    int socket_planificador;
     struct sockaddr_in dire_planificador;
-
-    struct timeval tv;      /* Para el timeout del accept */
-    socklen_t size_addr = 0;
+    fd_set rfds;
 
     char* port_planificador;
-    char* port_memoria; //Puerto de escucha
     char* ip_planificador;
-    char* ip_memoria;
-
-    t_config* config_cpu = config_create("Resources/config.cfg");
-
-    port_planificador= config_get_string_value(config_cpu, "PUERTO_PLANIFICADOR");
-    ip_planificador= config_get_string_value(config_cpu, "IP_PLANIFICADOR");
-
-    port_memoria= config_get_string_value(config_cpu, "PUERTO_MEMORIA");
-    ip_memoria= config_get_string_value(config_cpu, "IP_MEMORIA");
+    port_planificador=malloc(sizeof port_planificador);
+    ip_planificador=malloc(sizeof ip_planificador);
 
 
-    fd_set rfds;
+
+        t_config* config_cpu = config_create("config.cfg");
+        if (config_cpu != NULL){
+
+        port_planificador= config_get_string_value(config_cpu, "PUERTO_PLANIFICADOR");
+        ip_planificador= config_get_string_value(config_cpu, "IP_PLANIFICADOR");
+        }
+
+
+
     dire_planificador.sin_family = AF_INET;
     dire_planificador.sin_addr.s_addr = inet_addr(ip_planificador);
 	dire_planificador.sin_port = htons(port_planificador);
 
-	fd_set rfds2;
-	dire_memoria.sin_family = AF_INET;
-	dire_memoria.sin_addr.s_addr = inet_addr(ip_memoria);
-	dire_memoria.sin_port = htons(port_memoria);
-
-		int socket_planificador = socket(AF_INET, SOCK_STREAM, 0);
-		if (connect(socket_planificador, (void*) &dire_planificador, sizeof(dire_planificador)) != 0) {
+		int planificador = socket(AF_INET, SOCK_STREAM, 0);
+		if (connect(planificador, (void*) &dire_planificador, sizeof(dire_planificador)) != 0) {
 			perror("No se pudo conectar");
 			return 1;
 		}
 
-		int socket_memoria = socket(AF_INET, SOCK_STREAM, 0);
-			if (connect(socket_planificador, (void*) &dire_memoria, sizeof(dire_memoria)) != 0) {
-				perror("No se pudo conectar");
-				return 1;
-			}
-
 
 			char mensaje[1000]="";
-			recv(socket_planificador, mensaje,sizeof mensaje,0);
+			recv(planificador, mensaje,sizeof mensaje,0);
 			printf("Recibi mensaje: %s \n", mensaje);
-			send(socket_memoria,mensaje,sizeof mensaje,0);
 
 
 		return 0;
