9307565620b32dad0cca40537ba723caee5a3806
SebaAgosta <seba.agosta@gmail.com>
2015-09-10 17:24:40 -0300

    Planificador limpio

--- ProcessPlanificador/src/Cache13-CascaNuts-ProcessPlanificador.c
+++ ProcessPlanificador/src/Cache13-CascaNuts-ProcessPlanificador.c
@@ -22,196 +22,8 @@
 #include <net/if.h>
 #include <commons/config.h>
 
+void shell(int listener, int skEmisor, int skReceptor, char * buf, int nbytes);
 
-
-
-int isNum(char ch);
-
-int getPosStr(char *dest, char *buf, char sep, int index) {
-    char *ret;
-    char *ptr;
-    int indexCoincidencia = 0, i = 0;
-    ret = dest;
-
-    ret[0] = '\0';
-    for (ptr = buf; *ptr != '\0'; ptr++) {
-        if (indexCoincidencia > index)
-            return indexCoincidencia;
-        if (indexCoincidencia == index && sep != *ptr) {
-            ret[i++] = *ptr;
-            ret[i] = '\0';
-        }
-        if (*ptr == sep)
-            indexCoincidencia++;
-    }
-    return indexCoincidencia;
-}
-
-int strIsNum(char *ch) {
-    char *ptr;
-    for (ptr = ch; *ptr != '\0'; ptr++) {
-        if (!isNum(*ptr))
-            return 0;
-    }
-    return 1;
-}
-
-int isNum(char ch) {
-    if (ch >= '0' && ch <= '9')
-        return 1;
-    return 0;
-}
-
-/* ret: 1 MAch 0 No Mach >0 Error*/
-int regexpMach(char *expReg, char *txt) {
-    regex_t regex;
-    int reti;
-
-    /* Compile and validate regular expression */
-    if (regcomp(&regex, expReg, REG_EXTENDED | REG_NOSUB))
-        return -1;
-
-    /* Execute regular expression */
-    reti = regexec(&regex, txt, 0, NULL, 0);
-    if (!reti) {
-        regfree(&regex);
-        return 1;
-    } else if (reti == REG_NOMATCH) {
-        return 0;
-    } else {
-        return -2;
-    }
-    /* Free compiled regular expression if you want to use the regex_t again */
-    regfree(&regex);
-    return -3;
-}
-
-int lenParam(char *str, char *tok) {
-    char *p;
-    int i = 0;
-    p = strtok(str, tok);
-    while (p != NULL ) {
-        i++;
-        p = strtok(NULL, ",");
-    }
-    return i;
-}
-
-int split(char *str, char **splitstr, char *tok) {
-    char *p;
-    int i;
-
-    i = 0;
-    p = strtok(str, tok);
-    while (p != NULL ) {
-        splitstr[i] = malloc(strlen(p) + 1);
-        if (splitstr[i])
-            strcpy(splitstr[i], p);
-        i++;
-        p = strtok(NULL, tok);
-    }
-    return i;
-}
-
-int StrToInt(char * string)
-{
-    int i, n, temp = 0;
-    for (i = 0; i < strlen(string); i++)
-    {
-        if (  ( (int)string[i]< 48 ) || ( (int)string[i]>57 )  ) // Si el caracter es distinto a un numero
-        {
-            return -1; //Salimos y retornamos -1
-        }
-        else
-        {
-            for (n = 48; n <= 57; n++) // Hacemos un bucle desde el numero 0 al 9 basandonos en su valor ASCII
-            {
-                if (string[i] == n) // Si el valor ASCII del caracter actual correspondi al valor ASCII de un numero
-                {
-                    temp += (string[i] - 48); // Obtiene y suma el número actual al buffer
-                    temp *= 10; // Aumenta una cifra al resultado
-                }
-            }
-        }
-    }
-    return temp / 10; // Retorna el buffer, quitandole la última cifra que sobra
-}
-
-
-
-char * IntToStr(int number)
-{
-    int num, len = 0, i = 0, altnumber = number;
-    char * buffer;
-    if (number == 0)
-    {
-        return "0";
-    }
-    else
-    {
-        while (number > 0)
-        {
-           num = number%10;
-           number = number / 10;
-           len++;
-        }
-        buffer = (char *) malloc (len);
-        memset(buffer,0,strlen(buffer));
-        num = 0;
-        i = len -1;
-        while (altnumber> 0)
-        {
-           num = altnumber%10;
-           altnumber = altnumber / 10;
-           buffer[i] = (num + 48);
-           i--;
-        }
-        return buffer;
-    }
-
-
-}
-
-
-
-
-void shell(int listener, int skEmisor, int skReceptor, char * buf, int nbytes){
-
-   // char** comando = (char**)malloc(sizeof(char) * 1024 * 255);
-
-    //char action [255] ;
-   // char cpu [255] ;
-    //char msg[255] ;
-    //getPosStr(action,buf, ',',0);
-
-    printf("%s\n", buf);//action);
-
-/*
-
-    if (strncmp (action, "run", 3) == 0){
-        getPosStr(cpu,buf, ',',1);
-
-        printf("%s\n", cpu);
-
-        int skCpu = atoi(cpu);
-
-        if (skCpu == skReceptor){
-        getPosStr(msg,buf, ',',2);
-        printf("%s\n", msg);
-            if (send(skCpu, msg, nbytes, 0) == -1) {
-                perror("send");
-            }
-        }
-
-    }
-
-
-    if (strncmp (buf, "ls", 2) == 0){
-        printf("planificador\n");
-    }
-*/
-
-}
 
 
 // get sockaddr, IPv4 or IPv6:
@@ -249,11 +61,11 @@ int i;
 int main(void)
 {
 	//Espacio para la configuracion del entorno---------------------------<<
-	char* port; //Puerto de escucha
+	/*char* port; //Puerto de escucha
 	t_config* config_panificador;
 	config_panificador = config_create("Resources/Config.cfg");
 	port= config_get_string_value(config_panificador, "PORT");
-
+*/
 	//----------Soy una barra separadora ;)--------------------------------------//
 
 	fd_set master;    // master file descriptor list
@@ -284,7 +96,7 @@ int main(void)
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
-    if ((rv = getaddrinfo(NULL, port, &hints, &ai)) != 0) {
+    if ((rv = getaddrinfo(NULL, "9034", &hints, &ai)) != 0) {
         fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
         exit(1);
     }
@@ -396,4 +208,14 @@ int main(void)
 
     return 0;
 }
+
+
+void shell(int listener, int skEmisor, int skReceptor, char * buf, int nbytes){
+
+
+    printf("%s\n", buf);//action);
+
+
+}
+
 
