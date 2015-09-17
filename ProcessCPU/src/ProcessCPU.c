07413716bd8ebf4ffb0677f40bbbc0c5a02add39
Florencia Lopez <florenciamacarenalopez@hotmail.com.ar>
2015-09-17 14:11:29 -0300

    library para la cpu!

--- ProcessCPU/src/ProcessCPU.c
+++ ProcessCPU/src/ProcessCPU.c
@@ -13,73 +13,42 @@
 #include <pthread.h>
 #include <string.h>
 #include <commons/config.h>
+#include <cliente.h>
+#include <pthread.h>
+
 
 int main(void) {
 
-	 int puerto_escucha_planificador;
-	 char* ip_conec_plani;
-	 ip_conec_plani= malloc(sizeof ip_conec_plani);
+	pthread_t unHilito;
+
+
+	int puerto_escucha_planificador;
+	char* ip_conec_plani;
+	ip_conec_plani= malloc(sizeof ip_conec_plani);
 	 t_config* config;
 
-	                        	//puerto_escucha_planif=malloc(sizeof puerto_escucha_planif);
+	 int puerto_escucha_memoria;
+	 char* ip_conec_memoria;
+	 ip_conec_memoria= malloc(sizeof ip_conec_memoria);
+
+
 	                        	config = config_create("config.cfg");
 	                        	if(config != NULL){
 	                        	puerto_escucha_planificador=config_get_int_value(config, "PORT_PLANIFICADOR");
-	                        	ip_conec_plani=config_get_string_value(config,"IP_PLANIFICADOR");}
-
-
-    int socket_planificador;
-    struct sockaddr_in dire_serv;
-    fd_set rfds;
-    dire_serv.sin_family = AF_INET;
-    dire_serv.sin_addr.s_addr = inet_addr(ip_conec_plani);
-	dire_serv.sin_port = htons(puerto_escucha_planificador);
-
-		int planificador = socket(AF_INET, SOCK_STREAM, 0);
-		if (connect(planificador, (void*) &dire_serv, sizeof(dire_serv)) != 0) {
-			perror("No se pudo conectar");
-			return 1;
-		}
-
-
-			char mensaje[1000]="";
-			recv(planificador, mensaje,sizeof mensaje,0);
-			printf("Recibi mensaje: %s \n", mensaje);
+	                        	ip_conec_plani=config_get_string_value(config,"IP_PLANIFICADOR");
+	                        	puerto_escucha_memoria=config_get_int_value(config, "PORT_MEMORIA");
+	                        	ip_conec_memoria=config_get_string_value(config,"IP_MEMORIA");}
 
 
 
-//-----------------------conectandome con memoria y mandando msj--------------
+   int planificador = conectar_cliente(puerto_escucha_planificador, ip_conec_plani);
 
+   int memoria = conectar_cliente(puerto_escucha_memoria, ip_conec_memoria);
 
-		int puerto_escucha_memoria;
-		 char* ip_conec_memoria;
-		ip_conec_memoria= malloc(sizeof ip_conec_memoria);
-			 //t_config* config;
+   char* mensaje = recibirMensaje( planificador);
 
-			                        	//puerto_escucha_planif=malloc(sizeof puerto_escucha_planif);
-			                        	//config = config_create("config.cfg");
-			                        	//if(config != NULL){
-			                        	puerto_escucha_memoria=config_get_int_value(config, "PORT_MEMORIA");
-			                        	ip_conec_memoria=config_get_string_value(config,"IP_MEMORIA");
+   enviarMesaje(memoria, mensaje);
 
-
-
-		    struct sockaddr_in dire_memoria;
-
-		    dire_memoria.sin_family = AF_INET;
-		    dire_memoria.sin_addr.s_addr = inet_addr(ip_conec_memoria);
-			dire_memoria.sin_port = htons(puerto_escucha_memoria);
-
-				int memoria = socket(AF_INET, SOCK_STREAM, 0);
-				if (connect(memoria, (void*) &dire_memoria, sizeof(dire_memoria)) != 0) {
-					perror("No se pudo conectar");
-					return 1;
-				}
-
-
-
-					send(memoria, mensaje,sizeof mensaje,0);
-					printf("Envie mensaje: %s \n", mensaje);
 
 
 				return 0;
---
+++ conexion/cliente.c
@@ -0,0 +1,50 @@
+/*
+ * cliente.c
+ *
+ *  Created on: 17/9/2015
+ *      Author: utnso
+ */
+
+#ifndef CLIENTE_C_
+#define CLIENTE_C_
+#include <sys/socket.h>
+#include "cliente.h"
+#include <commons/config.h>
+
+int conectar_cliente(int puerto,char* ip){
+
+	int socket_planificador;
+	struct sockaddr_in dire_serv;
+	fd_set rfds;
+	dire_serv.sin_family = AF_INET;
+	dire_serv.sin_addr.s_addr = inet_addr(ip);
+	dire_serv.sin_port = htons(puerto);
+
+			int planificador = socket(AF_INET, SOCK_STREAM, 0);
+			if (connect(planificador, (void*) &dire_serv, sizeof(dire_serv)) != 0) {
+							perror("No se pudo conectar");
+
+			}
+
+			return planificador	;
+
+
+}
+char* recibirMensaje(int socket) {
+
+		char mensaje[1000]="";
+		recv(socket, mensaje,sizeof mensaje,0);
+		printf("Recibi mensaje: %s \n", mensaje);
+		return mensaje;
+}
+void enviarMesaje(int socket,char* mensaje) {
+
+	send(socket, mensaje,sizeof mensaje,0);
+	printf("Envie mensaje: %s \n", mensaje);
+
+	return;
+}
+
+
+
+#endif /* CLIENTE_C_ */
